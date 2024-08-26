3a1528a4eef0123d72cf23b08418e8a25b50df1e
Werbsson Saymon <64561641+WerbssonSaymon@users.noreply.github.com>
2024-08-26 09:44:20 -0300

    cadastro de usuarios e validação

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
   const [valor, setValor] = useState('');
   const [tipoAcao, setTipoAcao] = useState('')
   const [listaOperacoes, setListaOperacoes] = useState([]);
+  const [listaUsuarios, setListaUsuarios] = useState([])
+  const [nome, setNome] = useState('')
+  const [usuarioSelecionado, setUsuarioSelecionado] = useState('');
 
   useEffect(() => {
     const primeiraOperacao = {
@@ -18,6 +21,34 @@ function App() {
 
     setListaOperacoes([primeiraOperacao]);
   }, [])
+
+
+  function cadastrarUsuario(){
+
+    const usuarioIgual = listaUsuarios.find(usuario => usuario.nome.toLowerCase() === nome.toLowerCase())
+
+    if (usuarioIgual) {
+      alert('Já existe um usuário com esse nome.');
+      setNome('')
+      return;
+    }
+
+    if (nome === ""){
+      alert('Por favor, defina um nome');
+      setNome('')
+      return;
+    }
+
+    const novoUsuario = {
+      nome: nome
+    }
+
+    setListaUsuarios([...listaUsuarios, novoUsuario])
+    setNome('')
+
+  }
+
+
 
   function calcularSaldo() {
 
@@ -55,7 +86,7 @@ function App() {
         setValor('');
         return;
       }
-      
+
       if (valorInput > saldoAtual) {
         alert('Saldo insuficiente para o saque.');
         setValor('');
@@ -76,54 +107,118 @@ function App() {
 
   return (
     <>
-      <div className="container bg-body rounded">
-        
-        <Title titulo="caixa eletrônico"/>
-        
-        <span>{calcularSaldo()}</span>
+      <div className="container bg-body rounded col-lg-12">
 
-        <div className="d-grid gap-2">
-          <select className="form-select form-select-lg mb-3"
-                  value={tipoAcao}
-                  onChange={e => setTipoAcao(e.target.value)}>
-
-            <option value="DEFAULT">Escolha uma opção</option>
-            <option value="Sacar">Sacar</option>
-            <option value="Depositar">Depositar</option>
-          </select>
-
-          {tipoAcao && tipoAcao != "DEFAULT"  && (
-            <>
-              <input
-                type="number"
-                value={valor}
-                onChange={e => setValor(e.target.value)}
-              />
-              <button onClick={executarMovimentacao}>Confirmar</button>
-            </>
-          )}
+        <div className="row p-2 bg-primary">
+              {/* <p className="fs-3 pt-2 text-start">Bem vindo</p> */}
+              <p className="fs-3 pt-2 text-end text-white fw-semibold">Saldo atual: {calcularSaldo()}</p>
+              {/* {listaUsuarios.map((x, i) => ( 
+                <>
+                  <p key={i}></p>
+                  <p>{x.nome}</p>
+                </>
+              ))} */}
         </div>
-        
-        <h3 className="text-center">Historico de movimentação</h3>
 
-        <Table>
-          <thead>
-            <tr>     
-              <th>Operação</th>
-              <th>Valor</th>
-              <th>Data de Transação</th>
-            </tr>
-          </thead>
-          <tbody className="table-group-divider">
-            {listaOperacoes.map((x, i) => ( //variavel i de index pós meu objeto retornado não tem id pra usar na key
-              <tr key={i}>
-                <td>{x.operacao} </td>
-                <td>R$ {x.valor.toFixed(2)}</td> 
-                <td>{x.data}</td>
-              </tr>
-            ))} 
-          </tbody>
-        </Table>
+
+        <div className="row p-5">
+
+          <p>Cadastre o usuario</p>
+
+          <input
+            className="form-control"
+            type="text"
+            value={nome}
+            onChange={e => setNome(e.target.value)}
+          />
+          <button
+            className="btn btn-primary" 
+            onClick={cadastrarUsuario}>Confirmar
+          </button>
+
+          <select
+                className="form-select form-select-lg mb-3"
+                value={usuarioSelecionado}
+                onChange={e => setUsuarioSelecionado(e.target.value)}>
+                <option value="">Selecione um usuário</option>
+                {listaUsuarios.map((x, i) => (
+                  <option key={i} value={x.nome}>{x.nome}</option>
+                ))}
+              </select>
+
+        </div>
+
+
+
+
+
+        <div className="row p-5">
+
+          <div className="col-md-6 p-5">
+
+            <Title titulo="caixa eletrônico"/>
+
+            <div className="d-grid gap-2 mt-3">
+              <select className="form-select form-select-lg mb-3"
+                      value={tipoAcao}
+                      onChange={e => setTipoAcao(e.target.value)}>
+
+                  <option value="DEFAULT">Escolha uma opção</option>
+                  <option value="Sacar">Sacar</option>
+                  <option value="Depositar">Depositar</option>
+              </select>
+
+              {tipoAcao && tipoAcao != "DEFAULT"  && (
+                <>
+                  <input
+                    className="form-control"
+                    type="number"
+                    placeholder={tipoAcao}
+                    value={valor}
+                    onChange={e => setValor(e.target.value)}
+                  />
+                  <button
+                    className="btn btn-primary" 
+                    onClick={executarMovimentacao}>Confirmar</button>
+                </>
+              )}
+            </div>
+          </div>
+          
+          <div className="col-md-6 p-5">
+
+            <Title titulo="Historico de movimentação"></Title>
+            
+            <div style={{height: "400px", overflow: "scroll" }}>
+
+              <Table>
+              
+                <thead>
+                  <tr>     
+                    <th>Operação</th>
+                    <th>Valor</th>
+                    <th>Data de Transação</th>
+                  </tr>
+                </thead>
+                <tbody className="table-group-divider">
+                 
+                    {listaOperacoes.map((x, i) => ( //variavel i de index pós meu objeto retornado não tem id pra usar na key
+                      <tr key={i}>
+                        <td>{x.operacao} </td>
+                        <td>R$ {x.valor.toFixed(2)}</td> 
+                        <td>{x.data}</td>
+                      </tr>
+                    ))} 
+                  
+                </tbody>
+
+               
+              </Table>
+            </div>
+            
+          </div>
+
+        </div>
       </div>
         
     </>
--- src/componentes/title.jsx
+++ src/componentes/title.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function title(params) {
   return (
-    <div className='mt-5'>
-      <h1 className="fw-bolder text-center text-uppercase mt-5">{params.titulo}</h1>
+    <div>
+      <h1 className="fw-bolder fs-3 text-center text-uppercase mt-2">{params.titulo}</h1>
     </div>
   )
 }
